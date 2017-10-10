02cea799cf77a1725427ad2047943b1a218c19f4
vince <xiequan0759@163.com>
2017-10-10 17:30:55 +0800

    listview组件右侧入口快速实现

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,27 +1,66 @@
 <template>
   <scroll class="listview" ref="listview" :data="data">
     <ul>
-      <li v-for="group in data" class="list-group">
+      <li v-for="group in data" class="list-group" ref="listGroup">
         <h1 class="list-group-title">{{ group.title }}</h1>
         <ul>
           <li v-for="item in group.items" class="list-group-item">
-            <img class="avatar" :src="item.avatar">
+            <img class="avatar" v-lazy="item.avatar">
             <span class="name">{{ item.name }}</span>
           </li>
         </ul>
       </li>
     </ul>
+    <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" class="item" :data-index="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from '../scroll/scroll.vue'
+  import {getData} from '../../common/js/dom'
+
+  const ANCHOR_HEIGHT = 18
 
   export default {
+    created() {
+      this.touch = {}
+    },
     props: {
       data: {
         type: Array,
         default: []
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      }
+    },
+    methods: {
+      _scrollTo (index) {
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index])
+      },
+      onShortcutTouchStart (e) {
+        let anchorIndex = getData(e.target, 'index')
+        let firstTouch = e.touches[0]
+        this.touch.y1 = firstTouch.pageY
+        this.touch.anchorIndex = anchorIndex
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove (e) {
+        let firstTouch = e.touches[0]
+        this.touch.y2 = firstTouch.pageY
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+        this._scrollTo(anchorIndex)
       }
     },
     components: {
@@ -60,4 +99,23 @@
           margin-left: 20px
           font-size: $font-size-medium
           color: $color-text-l
+    .list-shortcut
+      position: absolute
+      z-index:30
+      right:0
+      top:50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
 </style>
