aea992524d88fcafd38bd73e2e9798221f3004af
vince <xiequan0759@163.com>
2017-10-17 23:12:47 +0800

    music-list组件

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div ref="playBtn" v-show="songs.length>0" class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -18,6 +28,12 @@
 <script type="text/ecmascript-6">
   import Scroll from '../../base/scroll/scroll.vue'
   import SongList from '../../base/song-list/song-list.vue'
+  import Loading from '../../base/loading/loading.vue'
+  import {prefixStyle} from '../../common/js/dom'
+
+  const RESERVED_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
 
   export default {
     props: {
@@ -45,16 +61,44 @@
     },
     mounted() {
       this.imageHeight = this.$refs.bgImage.clientHeight
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
       this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
       }
     },
     watch: {
       scrollY(newY) {
-        this.$refs.layer.style['transform'] = `translate3d(0, ${this.scrollY}px, 0)`
+        let zIndex = 0
+        let scale = 1
+        let blur = 0
+        let translateY = Math.max(this.minTranslateY, newY)
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(percent * 20, 20)
+        }
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
+        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
+        }
+        this.$refs.bgImage.style.zIndex = zIndex
       }
     },
     computed: {
@@ -64,7 +108,8 @@
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
@@ -108,6 +153,37 @@
       padding-top: 70%
       transform-origin: top
       background-size: cover
+      .play-wrapper
+        position: absolute
+        bottom: 20px
+        width: 100%
+        z-index: 50
+        .play
+          margin: 0 auto
+          padding: 7px 0
+          box-sizing: border-box
+          width: 135px
+          text-align: center
+          border:1px solid $color-theme
+          border-radius: 100px
+          color: $color-theme
+          font-size: 0
+          .icon-play
+            margin-right: 6px
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-medium-x
+          .text
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-small
+      .filter
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: rgba(7, 17, 27, 0.4)
     .bg-layer
       position: relative
       height:100%
@@ -120,4 +196,9 @@
       background: $color-background
       .song-list-wrapper
         padding:20px 30px
+      .loading-container
+        position: absolute
+        top: 50%
+        width: 100%
+        transform: translateY(-50%)
 </style>
