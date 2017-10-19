a7209c5eb0e0e929b18b063a256c7eb8e1793953
vince <xiequan0759@163.com>
2017-10-19 15:06:15 +0800

    歌曲数据的应用

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,33 +1,92 @@
 <template>
   <div class="player" v-show="playlist.length>0">
     <div class="normal-player" v-show="fullScreen">
-      normal player
+      <div class="background">
+        <img :src="currentSong.image" alt="" height="100%" width="100%">
+      </div>
+      <div class="top">
+        <div class="back" @click="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title" v-html="currentSong.name"></h1>
+        <h2 class="subtitle" v-html="currentSong.singer"></h2>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img class="image" :src="currentSong.image" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon icon-left">
+            <i :class="iconMode"></i>
+          </div>
+          <div class="icon icon-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon icon-center">
+            <i :class="playIcon"></i>
+          </div>
+          <div class="icon icon-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon icon-right">
+            <i class="icon"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-player" v-show="!fullScreen">
-      mini player
+    <div class="mini-player" v-show="!fullScreen" @click="open">
+      <div class="icon">
+        <img src="" alt="" width="40" height="40">
+      </div>
+      <div class="text">
+        <h2 class="name" v-html="currentSong.name"></h2>
+        <p class="desc" v-html="currentSong.singer"></p>
+      </div>
+      <div class="control">
+        <i class="icon-mini"></i>
+      </div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters} from 'vuex'
+  import {mapGetters, mapMutations} from 'vuex'
 
   export default {
     computed: {
       ...mapGetters([
         'fullScreen',
-        'currentIndex',
+        'currentSong',
         'playing',
         'playlist'
       ])
     },
     methods: {
+      back() {
+        this.setFullScreen(false)
+      },
+      open() {
+        this.setFullScreen(true)
+      },
+      ...mapMutations({
+        setFullScreen: 'SET_FULL_SCREEN'
+      })
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import '../../common/stylus/variable.styl'
+  @import '../../common/stylus/mixin.styl'
 
   .player
     .normal-player
@@ -38,6 +97,98 @@
       bottom: 0
       z-index: 150
       background: $color-background
+      .background
+        position: absolute
+        left: 0
+        top: 0
+        height: 100%
+        width: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 25px
+        .back
+          position: absolute
+          top: 0
+          left: 6px
+          z-index: 50
+          .icon-back
+            display: block
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          margin: 0 auto
+          width: 70%
+          nowrap()
+          line-height: 40px
+          text-align: center
+          font-size: $font-size-large
+          color: $color-text
+        .subtitle
+          line-height: 20px
+          text-align: center
+          font-size $font-size-medium
+          color: $color-text
+      .middle
+        position: fixed
+        top: 80px
+        bottom: 170px
+        width: 100%
+        white-space: nowrap
+        font-size: 0
+        .middle-l
+          display: inline-block
+          vertical-align: top
+          position: relative
+          width: 100%
+          height: 0
+          padding-top: 80%
+          .cd-wrapper
+            position: absolute
+            top: 0
+            left: 10%
+            width: 80%
+            height: 100%
+            .cd
+              width: 100%
+              height: 100%
+              box-sizing: border-box
+              border: 10px solid rgba(255, 255, 255, 0.1)
+              border-radius: 50%
+              .image
+                position: absolute
+                left: 0
+                top: 0
+                width: 100%
+                height: 100%
+                border-radius: 50%
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .operators
+          display: flex
+          align-items: center
+          .icon
+            flex: 1
+            color: $color-theme
+            i
+              font-size: 30px
+          .icon-left
+            text-align: right
+          .icon-center
+            padding: 0 20px
+            text-align center
+            i
+              font-size: 40px
+          .icon-right
+            text-align: left
+          .icon-favorite
+            color: $color-sub-theme
     .mini-player
       display: flex
       align-items: center
@@ -48,4 +199,38 @@
       height: 60px
       z-index: 180
       background: $color-highlight-background
+      .icon
+        flex:0 0 40px
+        width: 40px
+        padding:0 10px 0 20px
+        img
+          border-radius: 50%
+      .text
+        display: flex
+        flex-direction: column
+        justify-content: center
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+        .name
+          margin-bottom: 2px
+          nowrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          nowrap()
+          font-size: $font-size-small
+          color: $color-text-d
+      .control
+        flex:0 0 30px
+        width: 30px
+        padding: 0 10px
+        .icon-play-mini, .icon-pause-mini, .icon-playlist
+          font-size: 30px
+          color: $color-theme-d
+        .icon-mini
+          position: absolute
+          top: 0
+          left: 0
+          font-size: 32px
 </style>
