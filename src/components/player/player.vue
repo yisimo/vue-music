caddddf2fe02e7b8439f2a752f324ed45069e3e3
vince <xiequan0759@163.com>
2017-10-28 11:46:12 +0800

    播放器歌词左右滑动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -16,26 +16,33 @@
           <h1 class="title" v-html="currentSong.name"></h1>
           <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
-        <div class="middle">
-        <!--  <div class="middle-l">
+        <div class="middle"
+             @touchstart.prevent="middleTouchStart"
+             @touchmove.prevent="middleTouchMove"
+             @touchend="middleTouchEnd">
+          <div class="middle-l" ref="middleL">
             <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd" :class="cdCls">
                 <img class="image" :src="currentSong.image" alt="">
               </div>
             </div>
-          </div> -->
+          </div>
           <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines">
             <div class="lyric-wrapper">
               <div v-if="currentLyric">
                 <p ref="lyricLine"
-                  class="text"
-                  :class="{'current': currentLineNum === index}"
-                  v-for="(line, index) in currentLyric.lines">{{ line.txt }}</p>
+                   class="text"
+                   :class="{'current': currentLineNum === index}"
+                   v-for="(line, index) in currentLyric.lines">{{ line.txt }}</p>
               </div>
             </div>
           </scroll>
         </div>
         <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+            <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
+          </div>
           <div class="progress-wrapper">
             <span class="time time-l">{{ farmat(currentTime) }}</span>
             <div class="progress-bar-wrapper">
@@ -92,10 +99,10 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters, mapMutations} from 'vuex'
-  import {prefixStyle} from '../../common/js/dom'
-  import {playMode} from '../../common/js/config'
-  import {shuffle} from '../../common/js/util'
+  import { mapGetters, mapMutations } from 'vuex'
+  import { prefixStyle } from '../../common/js/dom'
+  import { playMode } from '../../common/js/config'
+  import { shuffle } from '../../common/js/util'
   import animations from 'create-keyframe-animation'
   import ProgressBar from '../../base/progress-bar/progress-bar.vue'
   import ProgressCircle from '../../base/progress-circle/progress-circle.vue'
@@ -103,6 +110,7 @@
   import Scroll from '../../base/scroll/scroll.vue'
 
   const transform = prefixStyle('transform')
+  const transitionDuration = prefixStyle('transitionDuration')
 
   export default {
     data() {
@@ -111,8 +119,12 @@
         currentTime: 0,
         radius: 32,
         currentLyric: null,
-        currentLineNum: 0
+        currentLineNum: 0,
+        currentShow: 'cd'
       }
+    },
+    created() {
+      this.touch = {}
     },
     computed: {
       percent() {
@@ -144,6 +156,58 @@
       ])
     },
     methods: {
+      middleTouchStart(e) {
+        this.touch.initiated = true
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY
+      },
+      middleTouchMove(e) {
+        if (!this.touch.initiated) {
+          return
+        }
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        const deltaY = touch.pageY - this.touch.startY
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+          return
+        }
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+        this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = 0
+        this.$refs.middleL.style.opacity = 1 - this.touch.percent
+        this.$refs.middleL.style[transitionDuration] = 0
+      },
+      middleTouchEnd() {
+        let offsetWidth
+        let opacity
+        const time = 300
+        if (this.currentShow === 'cd') {
+          if (this.touch.percent > 0.1) {
+            offsetWidth = -window.innerWidth
+            this.currentShow = 'lyric'
+            opacity = 0
+          } else {
+            offsetWidth = 0
+            opacity = 1
+          }
+        } else {
+          if (this.touch.percent < 0.9) {
+            offsetWidth = 0
+            opacity = 1
+            this.currentShow = 'cd'
+          } else {
+            opacity = 0
+            offsetWidth = -window.innerWidth
+          }
+        }
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = `${time}ms`
+        this.$refs.middleL.style.opacity = opacity
+        this.$refs.middleL.style[transitionDuration] = `${time}ms`
+      },
       back() {
         this.setFullScreen(false)
       },
@@ -309,7 +373,8 @@
         return {
           x,
           y,
-          scale}
+          scale
+        }
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN',
@@ -452,6 +517,21 @@
         position: absolute
         bottom: 50px
         width: 100%
+        .dot-wrapper
+          text-align: center
+          font-size: 0
+          .dot
+            display: inline-block
+            vertical-align: middle
+            margin: 0 4px
+            width: 8px
+            height: 8px
+            border-radius: 50%
+            background: $color-text-l
+            &.active
+              width: 20px
+              border-radius: 5px
+              background: $color-text-ll
         .progress-wrapper
           display: flex
           align-items: center
@@ -459,7 +539,7 @@
           padding: 10px 0
           width: 80%
           .time
-            flex:0 0 30px
+            flex: 0 0 30px
             width: 30px
             line-height: 30px
             color: $color-text
@@ -505,16 +585,16 @@
       display: flex
       align-items: center
       position: fixed
-      left:0
-      bottom:0
+      left: 0
+      bottom: 0
       width: 100%
       height: 60px
       z-index: 180
       background: $color-highlight-background
       .icon
-        flex:0 0 40px
+        flex: 0 0 40px
         width: 40px
-        padding:0 10px 0 20px
+        padding: 0 10px 0 20px
         img
           border-radius: 50%
       .text
@@ -534,7 +614,7 @@
           font-size: $font-size-small
           color: $color-text-d
       .control
-        flex:0 0 30px
+        flex: 0 0 30px
         width: 30px
         padding: 0 10px
         .icon-play-mini, .icon-pause-mini, .icon-playlist
@@ -549,6 +629,7 @@
         transition: all 0.4s
       &.mini-enter, &.mini-leave-to
         opacity: 0
+
   @keyframes rotate
     0%
       transform: rotate(0)
