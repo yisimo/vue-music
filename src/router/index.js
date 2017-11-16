fd88082f0fbd6140062da99083bd19767508877f
vince <xiequan0759@163.com>
2017-11-16 11:12:52 +0800

    通过异步加载优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from '../components/recommend/recommend.vue'
-import Rank from '../components/rank/rank.vue'
-import Singer from '../components/singer/singer.vue'
-import Search from '../components/search/search.vue'
-import SingerDetail from '../components/singer-detail/singer-detail.vue'
-import Disc from '../components/disc/disc.vue'
-import TopList from '../components/top-list/top-list.vue'
-import UserCenter from '../components/user-center/user-center.vue'
 
 Vue.use(Router)
+
+const Recommend = (resolve) => {
+  import('../components/recommend/recommend.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const Rank = (resolve) => {
+  import('../components/rank/rank.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const Singer = (resolve) => {
+  import('../components/singer/singer.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const Search = (resolve) => {
+  import('../components/search/search.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('../components/singer-detail/singer-detail.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const Disc = (resolve) => {
+  import('../components/disc/disc.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const TopList = (resolve) => {
+  import('../components/top-list/top-list.vue').then((module) => {
+    resolve(module)
+  })
+}
+
+const UserCenter = (resolve) => {
+  import('../components/user-center/user-center.vue').then((module) => {
+    resolve(module)
+  })
+}
 
 export default new Router({
   routes: [
